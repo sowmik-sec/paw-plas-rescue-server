7a961e2c8176ffbd7cf4ced38c2ed0119e965fb0
sowmik-sec <sowmik.sec@gmail.com>
2024-09-15 21:52:59 +0600

    set mongodb and save user to db

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 var cors = require("cors");
 require("dotenv").config();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Paw pals rescue server is running");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgh8h2c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
+async function run() {
+  try {
+    const userCollection = client.db("pawPalsRescue").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const isExists = await userCollection.findOne(query);
+      if (isExists) {
+        return res.send({ message: "User already exists", insertedId: null });
+      } else {
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      }
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Paw pals rescue is running!");
+});
 app.listen(port, () => {
   console.log(`Paw pals rescue server is listening on port ${port}`);
 });
