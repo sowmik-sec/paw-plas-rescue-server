7b33aae91dbbd718088e6cb9bff23a30e1a84c30
sowmik-sec <sowmik.sec@gmail.com>
2024-09-19 21:27:00 +0600

    create verifyToken middleware

--- index.js
+++ index.js
@@ -34,6 +34,21 @@ async function run() {
       .db("pawPalsRescue")
       .collection("petCategories");
 
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
     // jwt related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
